fef66fb92c498f0421a6f126287a6634de1d607d
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2025-04-10 09:15:45 -0300

    Projeto criado!

---
+++ src/main/java/br/com/diademaenforma/enformaplus/EnformaplusApplication.java
@@ -0,0 +1,13 @@
+package br.com.diademaenforma.enformaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnformaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnformaplusApplication.class, args);
+	}
+
+}
